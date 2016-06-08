231f893446438eb558e486141b0197265ba4e79a
AlcinaW <white_kaitou@hotmail.com>
2016-06-07 23:57:32 -0400

    plane test

--- js/app.js
+++ js/app.js
@@ -1,67 +1,75 @@
-var Colors = {
+ var Colors = {
 	red:0xf25346,
 	white:0xd8d0d1,
 	brown:0x59332e,
 	pink:0xF5986E,
 	brownDark:0x23190f,
-	blue:0x68c3c0,
+	blue:0x757575, //planet
 };
 
-
-window.addEventListener("load", init, false);
+window.addEventListener('load', init, false);
 
 function init() {
-	// set up scene, camera and renderer
 	createScene();
 
-	// add lights
 	createLights();
 
-	// add objects
-	//createGundam();
-	createPlanet();
+	createPlane();
+	createSea();
 	createSky();
 
-	// start a loop that will update the objects' positions, and render the scene on each frame
 	loop();
 }
 
-var scene, camera, fieldOfView, aspectRatio, nearGundam, farGundam, HEIGHT, WIDTH, renderer, container;
+var scene,
+		camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH,
+		renderer, container;
 
 function createScene() {
-  HEIGHT = window.innerHeight;
-  WIDTH = window.innerWidth;
 
-  scene = new THREE.Scene();
+	HEIGHT = window.innerHeight;
+	WIDTH = window.innerWidth;
 
-  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+	scene = new THREE.Scene();
 
-  aspectRatio = WIDTH / HEIGHT;
-  fieldOfView = 60;
-  nearGundam = 1;
-  farGundam = 10000;
-  camera = new THREE.PerspectiveCamera(
-  fieldOfView,
-  aspectRatio,
-  nearGundam,
-  farGundam
-  );
+	scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
 
-  camera.position.x = 0;
-  camera.position.z = 200;
-  camera.position.y = 100;
+	// Create the camera
+	aspectRatio = WIDTH / HEIGHT;
+	fieldOfView = 60;
+	nearPlane = 1;
+	farPlane = 10000;
+	camera = new THREE.PerspectiveCamera(
+		fieldOfView,
+		aspectRatio,
+		nearPlane,
+		farPlane
+		);
 
-  // Create the renderer
-  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+	camera.position.x = 0;
+	camera.position.z = 200;
+	camera.position.y = 100;
 
-  renderer.setSize(WIDTH, HEIGHT);
+	renderer = new THREE.WebGLRenderer({
+		alpha: true,
+		antialias: true
+	});
 
-  renderer.shadowMap.enabled = true;
+	renderer.setSize(WIDTH, HEIGHT);
+	renderer.shadowMap.enabled = true;
 
-  container = document.getElementById('world');
-  container.appendChild(renderer.domElement);
+	container = document.getElementById('world');
+	container.appendChild(renderer.domElement);
 
-  window.addEventListener('resize', handleWindowResize, false);
+	window.addEventListener('resize', handleWindowResize, false);
+}
+
+function handleWindowResize() {
+	HEIGHT = window.innerHeight;
+	WIDTH = window.innerWidth;
+	renderer.setSize(WIDTH, HEIGHT);
+	camera.aspect = WIDTH / HEIGHT;
+	camera.updateProjectionMatrix();
 }
 
 var hemisphereLight, shadowLight;
@@ -72,8 +80,8 @@ function createLights() {
 
 	shadowLight = new THREE.DirectionalLight(0xffffff, .9);
 
-	// set direction of light
 	shadowLight.position.set(150, 350, 350);
+
 	shadowLight.castShadow = true;
 
 	shadowLight.shadow.camera.left = -400;
@@ -83,7 +91,6 @@ function createLights() {
 	shadowLight.shadow.camera.near = 1;
 	shadowLight.shadow.camera.far = 1000;
 
-	// define resolution of the shadow
 	shadowLight.shadow.mapSize.width = 2048;
 	shadowLight.shadow.mapSize.height = 2048;
 
@@ -91,8 +98,7 @@ function createLights() {
 	scene.add(shadowLight);
 }
 
-//define planet
-Planet = function(){
+Sea = function(){
 
 	var geom = new THREE.CylinderGeometry(600,600,800,40,10);
 
@@ -110,83 +116,101 @@ Planet = function(){
 	this.mesh.receiveShadow = true;
 }
 
-var planet;
+var sea;
 
-function createPlanet(){
-	planet = new Planet();
+function createSea(){
+	sea = new Sea();
 
-	// push it a little bit at the bottom of the scene
-	planet.mesh.position.y = -600;
+	sea.mesh.position.y = -600;
 
-	// add the mesh of the sea to the scene
-	scene.add(planet.mesh);
+	scene.add(sea.mesh);
 }
 
 Cloud = function(){
-
+	// Create an empty container that will hold the different parts of the cloud
 	this.mesh = new THREE.Object3D();
 
+	// create a cube geometry;
+	// this shape will be duplicated to create the cloud
 	var geom = new THREE.BoxGeometry(20,20,20);
 
+	// create a material; a simple white material will do the trick
 	var mat = new THREE.MeshPhongMaterial({
 		color:Colors.white,
 	});
 
+	// duplicate the geometry a random number of times
 	var nBlocs = 3+Math.floor(Math.random()*3);
 	for (var i=0; i<nBlocs; i++ ){
 
+		// create the mesh by cloning the geometry
 		var m = new THREE.Mesh(geom, mat);
 
+		// set the position and the rotation of each cube randomly
 		m.position.x = i*15;
 		m.position.y = Math.random()*10;
 		m.position.z = Math.random()*10;
 		m.rotation.z = Math.random()*Math.PI*2;
 		m.rotation.y = Math.random()*Math.PI*2;
 
+		// set the size of the cube randomly
 		var s = .1 + Math.random()*.9;
 		m.scale.set(s,s,s);
 
+		// allow each cube to cast and to receive shadows
 		m.castShadow = true;
 		m.receiveShadow = true;
 
+		// add the cube to the container we first created
 		this.mesh.add(m);
 	}
 }
 
-// define sky
+// Define a Sky Object
 Sky = function(){
-
+	// Create an empty container
 	this.mesh = new THREE.Object3D();
 
-	// choose number of clouds to be scattered in the sky
+	// choose a number of clouds to be scattered in the sky
 	this.nClouds = 20;
 
-	// To distribute the clouds consistently, need to place them according to uniform angle
+	// To distribute the clouds consistently,
+	// we need to place them according to a uniform angle
 	var stepAngle = Math.PI*2 / this.nClouds;
 
 	// create the clouds
 	for(var i=0; i<this.nClouds; i++){
 		var c = new Cloud();
 
-		// set the rotation and the position of each cloud
-		var a = stepAngle*i; // final angle of the cloud
-		var h = 750 + Math.random()*200; // distance between the center of the axis and the cloud itself
+		// set the rotation and the position of each cloud;
+		// for that we use a bit of trigonometry
+		var a = stepAngle*i; // this is the final angle of the cloud
+		var h = 750 + Math.random()*200; // this is the distance between the center of the axis and the cloud itself
 
-		// trig, converting polar coordinates (angle, distance) into Cartesian coordinates (x, y)
+		// Trigonometry!!! I hope you remember what you've learned in Math :)
+		// in case you don't:
+		// we are simply converting polar coordinates (angle, distance) into Cartesian coordinates (x, y)
 		c.mesh.position.y = Math.sin(a)*h;
 		c.mesh.position.x = Math.cos(a)*h;
 
+		// rotate the cloud according to its position
 		c.mesh.rotation.z = a + Math.PI/2;
 
+		// for a better result, we position the clouds
+		// at random depths inside of the scene
 		c.mesh.position.z = -400-Math.random()*400;
 
-		// set a random scale for each cloud
+		// we also set a random scale for each cloud
 		var s = 1+Math.random()*2;
 		c.mesh.scale.set(s,s,s);
 
+		// do not forget to add the mesh of each cloud in the scene
 		this.mesh.add(c.mesh);
 	}
 }
+
+// Now we instantiate the sky and push its center a bit
+// towards the bottom of the screen
 
 var sky;
 
@@ -196,4 +220,85 @@ function createSky(){
 	scene.add(sky.mesh);
 }
 
+
+var AirPlane = function() {
+
+	this.mesh = new THREE.Object3D();
+
+	// Create the cabin
+	var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
+	var matCockpit = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+	var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+	cockpit.castShadow = true;
+	cockpit.receiveShadow = true;
+	this.mesh.add(cockpit);
+
+	// Create the engine
+	var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
+	var matEngine = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+	var engine = new THREE.Mesh(geomEngine, matEngine);
+	engine.position.x = 40;
+	engine.castShadow = true;
+	engine.receiveShadow = true;
+	this.mesh.add(engine);
+
+	// Create the tail
+	var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
+	var matTailPlane = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+	var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
+	tailPlane.position.set(-35,25,0);
+	tailPlane.castShadow = true;
+	tailPlane.receiveShadow = true;
+	this.mesh.add(tailPlane);
+
+	// Create the wing
+	var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
+	var matSideWing = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+	var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
+	sideWing.castShadow = true;
+	sideWing.receiveShadow = true;
+	this.mesh.add(sideWing);
+
+	// propeller
+	var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
+	var matPropeller = new THREE.MeshPhongMaterial({color:Colors.brown, shading:THREE.FlatShading});
+	this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
+	this.propeller.castShadow = true;
+	this.propeller.receiveShadow = true;
+
+	// blades
+	var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
+	var matBlade = new THREE.MeshPhongMaterial({color:Colors.brownDark, shading:THREE.FlatShading});
+
+	var blade = new THREE.Mesh(geomBlade, matBlade);
+	blade.position.set(8,0,0);
+	blade.castShadow = true;
+	blade.receiveShadow = true;
+	this.propeller.add(blade);
+	this.propeller.position.set(50,0,0);
+	this.mesh.add(this.propeller);
+};
+
+var airplane;
+
+function createPlane(){
+	airplane = new AirPlane();
+	airplane.mesh.scale.set(.25,.25,.25);
+	airplane.mesh.position.y = 100;
+	scene.add(airplane.mesh);
+}
+
 renderer.render(scene, camera);
+
+function loop(){
+	// Rotate the propeller, the sea and the sky
+	airplane.propeller.rotation.x += 0.3;
+	sea.mesh.rotation.z += .005;
+	sky.mesh.rotation.z += .01;
+
+	// render the scene
+	renderer.render(scene, camera);
+
+	// call the loop function again
+	requestAnimationFrame(loop);
+}
